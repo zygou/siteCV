da048d48813bc6b35650ea88da895d3cad62f583
zygou <jerome.zeghoudi@laposte.net>
2012-03-01 17:36:41 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SiteCV::Application.config.secret_token = '3b65626038b870071da194ecc53583a7f47e019dc5f97243853b87ab17a7c2bdb3d8579dbfa63d831953d4b3d004ac2ed45e7e6f4f5c96e584905d1d6dc27e8e'
